94164255e2f71bd6aa00a645c8e3cf64784c5abb
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2022-09-08 18:04:55 -0300

    Vanished cards
    Cards vanish and are no longer selectable once a match has been found.

--- memorygame/app.js
+++ memorygame/app.js
@@ -53,7 +53,10 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
-const cardChosen = [];
+let cardChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
+
 function createBoard() {
     for(let i = 0; i < cardArray.length; i++){
         const card = document.createElement('img');
@@ -62,22 +65,31 @@ function createBoard() {
         card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card);
     }
-}
+};
 
-createBoard()
+createBoard();
 
 function checkMatch(){
-    console.log('check for match!!');
+    const cards = document.querySelectorAll('img');
+
     if(cardChosen[0] == cardChosen[1]){
         alert('You found a match!');
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png');
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png');
+        cards[cardsChosenIds[0]].removeEventListener('click', flipCard);
+        cards[cardsChosenIds[1]].removeEventListener('click', flipCard);
+        cardsWon.push(cardChosen);
     }
-}
+    cardChosen = [];
+    cardsChosenIds = [];
+};
 
 function flipCard(){
     const cardId = this.getAttribute('data-id');
     cardChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId);
     this.setAttribute('src', cardArray[cardId].img);
     if (cardChosen.length === 2){
         setTimeout(checkMatch, 500);
     }
-}
+};
